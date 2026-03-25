38ad44f5975d0b0dd20985a60bbb93ae7533aa95
Bramtheking <bramwela8@gmail.com>
2026-03-25 12:43:21 +0300

    Newest commit

--- src/lib/rss.js
+++ src/lib/rss.js
@@ -4,9 +4,6 @@ import { db } from './firebase';
 import { collection, addDoc, query, where, getDocs, serverTimestamp } from 'firebase/firestore';
 
 const parser = new Parser({
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-  },
   customFields: {
     item: [
       ['media:content', 'mediaContent'],
